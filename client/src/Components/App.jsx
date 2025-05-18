5e7fd40387234fdb9b8431c166508b67555447b7
Likhith-18 <vparvathi222@gmail.com>
2025-05-18 11:34:41 +0530

    - Downgraded tailwindcss from 4.1.5 to stable 3.4.1 in devDependencies

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyForm from "./MyForm";
 import ImagePredict from "./ImagePredict";
+
 export default function App() {
   return (
     <Router>
