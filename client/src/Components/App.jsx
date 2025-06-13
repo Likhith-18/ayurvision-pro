a46323623e31d8180dc57686cd90e3e88c8b65d8
Likhith-18 <vparvathi222@gmail.com>
2025-06-13 23:44:20 +0530

    changed project structure move notebooks into ml-model folder

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Prakriti from "./Prakriti";
 import ImagePredict from "./ImagePredict";
-import NearbyDoctorsMap from '../pages/NearbyDoctersMap';
+import NearbyDoctorsMap from "../pages/NearbyDoctersMap";
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
           <Route path="/" Component={Home} />
           <Route path="/myform" Component={Prakriti} />
           <Route path="/image-predict" Component={ImagePredict} />
-          <Route path="/findDoctors" Component={NearbyDoctorsMap} />
+          <Route path="/find-doctors" Component={NearbyDoctorsMap} />
         </Routes>
       </div>
     </Router>
